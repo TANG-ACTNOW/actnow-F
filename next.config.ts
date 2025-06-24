a44dd7bd2ad24244cacc5036e614dcea959cbf66
HETANG404 <hetang401@gmail.com>
2025-06-24 13:40:52 +0800

    25.6.24.DailyUpdate: DONE.V1> CICD; TODO.V1> CICD TEST

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*',
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://localhost:8080/api/:path*',
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
